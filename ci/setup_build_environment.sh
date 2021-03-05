eb736b79b16e97e67b8744804cdbd2e0673552f6
Ryan Burn <ryan.burn@gmail.com>
2021-03-05 00:20:08 -0800

    set up ci

---
+++ ci/setup_build_environment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+export DEBIAN_FRONTEND="noninteractive"
+apt-get update 
+apt-get install --no-install-recommends --no-install-suggests -y \
+                software-properties-common \
+                build-essential \
+                libperl-dev \
+                libatlas-base-dev \
+                gfortran \
+                cmake \
+                pkg-config \
+                gettext-base \
+                zip \
+                git \
+                ca-certificates \
+                curl \
+                automake \
+                autogen \
+                autoconf \
+                libtool \
+                gnupg2 \
+                ssh \
+                lcov \
+                vim \
+                gdb \
+                wget
