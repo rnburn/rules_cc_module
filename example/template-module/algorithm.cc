275b43767cc3f58d468de874a71a61134fe14ddd
Ryan Burn <ryan.burn@gmail.com>
2021-03-06 18:55:51 -0800

    add template example

---
+++ example/template-module/algorithm.cc
@@ -0,0 +1,21 @@
+module;
+
+#include <algorithm>
+#include <cassert>
+#include <type_traits>
+#include <vector>
+
+export module algorithm;
+
+export template <class Iter>
+auto compute_median(Iter first, Iter last) {
+  using T = std::decay_t<decltype(*first)>;
+  auto n = std::distance(first, last);
+  assert(n > 0);
+  std::vector<const T*> v(n);
+  std::transform(first, last, v.begin(), [](auto& x) { return &x; });
+  auto mid = v.begin() + n / 2;
+  std::nth_element(v.begin(), mid, v.end(),
+                   [](const T* lhs, const T* rhs) { return *lhs < *rhs; });
+  return **mid;
+}
---
+++ example/template-module/main.cc
@@ -0,0 +1,12 @@
+import algorithm;
+
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> v1 = {1, -9, 7};
+  std::cout << compute_median(v1.begin(), v1.end()) << std::endl;
+  std::vector<double> v2 = {10.0, 5.0, 7.2, -3.5};
+  std::cout << compute_median(v2.begin(), v2.end()) << std::endl;
+  return 0;
+}
